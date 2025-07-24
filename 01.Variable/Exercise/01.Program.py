a765bf078b430f15ea300df7e92ab783efd77904
Vignesh061 <vigneshvicky3611@gmail.com>
2025-07-24 23:00:43 +0530

    Implemented Problems in Variable

---
+++ 01.Variable/Exercise/01.Program.py
@@ -0,0 +1,19 @@
+# Storing a value in a variable
+name="Vignesh"
+age=20
+height=4.5
+is_student=True
+
+# Printing a Variable
+print("Name:",name)
+print("Age:",age)
+print("Height:",height)
+print("is_student:",is_student)
+
+
+"""
+Name:Vignesh
+Age:20
+Height:4.5
+is_student:True
+"""
---
+++ 01.Variable/Exercise/02.Program.py
@@ -0,0 +1,8 @@
+
+a =10
+c=a+8
+a=20
+
+print("a+b:",c)
+# output a+b:8
+
