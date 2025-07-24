868da670365925332073506bcc8f3e277976b21c
Vignesh061 <vigneshvicky3611@gmail.com>
2025-07-24 23:01:11 +0530

    Variable Syntax

---
+++ 01.Variable/Syntax/syntax.py
@@ -0,0 +1,12 @@
+"""
+Vairale is a name used to store a date in a memory .You can use it to sotre a value , reuse, and modify it latter
+"""
+###      Syntax 
+###      Variable Name = Value
+
+variable_name="python"
+
+print(variable_name)
+
+### output
+### python
