a1144af6fd598f4d3b56094fd82ebc1a939ef5e1
Vignesh061 <vigneshvicky3611@gmail.com>
2025-07-24 22:58:43 +0530

    Data Type Syntax

---
+++ 02.Data_Types/Syntax.py
@@ -0,0 +1,36 @@
+# Data Types
+
+# | Data Type | Example Value | Python Example     |
+# | --------- | ------------- | ------------------ |
+# | `int`     | 10            | `x = 10`           |
+# | `float`   | 3.14          | `pi = 3.14`        |
+# | `str`     | "hello"       | `name = "Bob"`     |
+# | `bool`    | True or False | `is_admin = False` |
+
+
+# int
+x=10
+print(type(x))
+
+# float
+pi=3.14
+print(type(pi))
+
+# string
+name="vicky"
+print(type(name))
+ 
+# Boolean
+is_student=True
+print(type(is_student))
+
+
+"""  OUTPUT 
+
+
+<class 'int'>
+<class 'float'>
+<class 'str'>
+<class 'bool'>
+
+"""
