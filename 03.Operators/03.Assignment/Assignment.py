0ef0336a141286254a29b8231030ab9ba1809de2
Vignesh061 <vigneshvicky3611@gmail.com>
2025-07-25 23:05:48 +0530

    Assignment Operator

---
+++ 03.Operators/03.Assignment/Assignment.py
@@ -0,0 +1,14 @@
+# Assignment Operator
+
+""" Symbols
+=, +=, -=, *=, /=,
+"""
+
+a=10
+
+a+=5     #a=10+8  =15
+a-=6     #a=15-6  =9
+a*=2     #a=9*2   =18
+a/=2     #a=18/2  =6
+
+print("Result:",a)
