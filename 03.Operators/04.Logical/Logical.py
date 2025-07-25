3f28bc297b7ca54400f81d7edc3a56253df705da
Vignesh061 <vigneshvicky3611@gmail.com>
2025-07-25 23:33:01 +0530

    Logical Operator

---
+++ 03.Operators/04.Logical/Logical.py
@@ -0,0 +1,12 @@
+# Logical Operator
+
+""" Symbols
+ and, or, not
+"""
+
+a=True
+b=False
+
+print("a and b:",a and b)  # True and False =  False
+print("a ro b:",a or b)    # True or  False =  True
+print("not a:", not a )    # not True       =  False
