bfc25f413f8a8c49ac026099ea69fdd0a29b93d9
Vignesh061 <vigneshvicky3611@gmail.com>
2025-07-25 23:10:15 +0530

    Arithmetic Operator

---
+++ 03.Operators/01.Arithmetic/Arithmetic.py
@@ -0,0 +1,15 @@
+# Arithmetoc Operators 
+""" Symbols
+ +, -, *, /, //, %, **
+"""
+
+a=10
+b=4
+
+print("Addition:",a+b)          # 14
+print("Subtraction:",a-b)       # 6
+print("Multiplication:",a*b)    # 40
+print("Division:",a/b)          # 2.5
+print("Floor Divison:",a//b)    # 2
+print("Modulus:",a%b)           # 2
+print("Power:",a**b)            # 10000
