e3a7ffefd310002caec09371d12a514380d9c80d
Vignesh061 <vigneshvicky3611@gmail.com>
2025-07-25 23:06:58 +0530

    Comparision Operator

---
+++ 03.Operators/02.Comparision/Comparision.py
@@ -0,0 +1,14 @@
+# Comparisoin Operators
+
+"""Symbols 
+==, !=, <, >, <=, >=
+"""
+a=10
+b=2
+
+print(" a Equal to b :",a==b)                           #False
+print("a Not Equal b : ",a!=b)                          #True
+print(" a Less then b : ",a<b)                          #False
+print(" a Greter then b :",a>b)                         #True
+print("a Less the are equal to b :",a<=b)               #False
+print("a Greater then are equal to b :",a>=b)           #True
