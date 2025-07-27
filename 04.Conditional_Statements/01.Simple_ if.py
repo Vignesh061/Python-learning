caf6baeebfef3175cf642cc6215addf8937d5f6d
Vignesh P <vigneshvicky3611@gmail.com>
2025-07-27 23:23:33 +0530

    Simple if Syntax

---
+++ 04.Conditional_Statements/01.Simple_ if.py
@@ -0,0 +1,17 @@
+## simple if Syntax 
+
+
+# IF statement
+# if condition:
+    # code block
+
+# Program 
+
+num = 10
+
+if num > 0:
+    print("Number is positive")
+
+""" Output 
+Number is positive 
+"""
