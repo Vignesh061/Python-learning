8549f17ee9a485105a7b005d0d0dc0479bd3fd4e
Vignesh061 <vigneshvicky3611@gmail.com>
2025-07-28 21:40:37 +0530

    If Else

---
+++ 04.Conditional_Statements/02.If_Else/02.If_Else.py
@@ -0,0 +1,18 @@
+"""Syntax
+
+if condition:
+    # code if condition is True
+else:
+    # code if condition is False
+
+"""
+
+
+
+
+
+age = 16
+if age >= 18:
+    print("You can vote.")
+else:
+    print("You are not eligible to vote.")
