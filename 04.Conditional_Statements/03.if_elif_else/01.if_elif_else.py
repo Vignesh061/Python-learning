d55ea3f542bfe8d3dfb3e14258af6c0294d4b047
Vignesh061 <vigneshvicky3611@gmail.com>
2025-07-29 19:41:35 +0530

    if elif else

---
+++ 04.Conditional_Statements/03.if_elif_else/01.if_elif_else.py
@@ -0,0 +1,19 @@
+""" Syntax
+if condition1:
+    # code block
+elif condition2:
+    # code block
+else:
+    # default code block
+
+"""
+marks = 85
+
+if marks >= 90:
+    print("Grade: A")
+elif marks >= 75:
+    print("Grade: B")
+elif marks >= 60:
+    print("Grade: C")
+else:
+    print("Grade: F")
