2fb75569a58fa014f5744f7824763a76250685dc
Vignesh061 <vigneshvicky66005@gmail.com>
2025-08-03 21:52:59 +0530

    if elif else

--- 04.Conditional_Statements/03.if_elif_else/01.if_elif_else.py
+++ 04.Conditional_Statements/03.if_elif_else/01.if_elif_else.py
@@ -12,8 +12,9 @@ marks = 85
 if marks >= 90:
     print("Grade: A")
 elif marks >= 75:
-    print("Grade: B")
+     print("Grade: B")   
 elif marks >= 60:
     print("Grade: C")
 else:
     print("Grade: F")
+    
