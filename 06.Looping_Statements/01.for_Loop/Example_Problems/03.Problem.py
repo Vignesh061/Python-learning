49c601b5b7607ca7d762832383041709cc0b2fed
Vignesh061 <vigneshvicky3611@gmail.com>
2025-08-06 23:00:52 +0530

    Implemented Problems using for loop

---
+++ 06.Looping_Statements/01.for_Loop/Example_Problems/03.Problem.py
@@ -0,0 +1,4 @@
+# Print a  multiplication talbe for 2
+num=int(input("Enter a table number:"))
+for i in range (1,11):
+    print(i,f"X {num} = ",i*num)
---
+++ 06.Looping_Statements/01.for_Loop/Example_Problems/06.Problem.py
@@ -0,0 +1,6 @@
+# print all even numbers form  1 to 50
+print("Even numbers")
+for i in range (1,51):
+    if i%2==0:
+        print(i)
+    
