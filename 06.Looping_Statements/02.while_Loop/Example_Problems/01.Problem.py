397779290d9e3f2deeaf322f44d2724973e9a122
Vignesh061 <vigneshvicky3611@gmail.com>
2025-08-14 23:29:31 +0530

    Implemented Problem using while loop

---
+++ 06.Looping_Statements/02.while_Loop/Example_Problems/01.Problem.py
@@ -0,0 +1,9 @@
+# Print the first 10 even numbers.
+
+count = 1   # To keep track of how many even numbers printed
+num = 2     # First even number
+
+while count <= 10:
+    print(num)
+    num += 2     # Move to next even number
+    count += 1   # Increment count
