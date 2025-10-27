42f7aeb15bf6145ea773fe4d5de4024169f739f8
Vignesh061 <vigneshvicky3611@gmail.com>
2025-10-27 22:30:19 +0530

    Implemented Problem using nested for loop

---
+++ 06.Looping_Statements/03.Nested_for_loop/Example_Problems/01.Problem.py
@@ -0,0 +1,7 @@
+for i in range(0,6):
+    for j in range(i,6):
+        print("*" ,end=" ")
+    print()    
+        
+
+    
---
+++ 06.Looping_Statements/03.Nested_for_loop/Example_Problems/04.Problem.py
@@ -0,0 +1,6 @@
+for i in range(0,6):
+    for j in range(i):
+        print(" ",end=" ")
+    for k in range(i,6):
+        print("*",end=" ")
+    print()
