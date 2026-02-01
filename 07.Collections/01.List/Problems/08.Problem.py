8d725728ac0a641bbab60f10816a4cd67acf6a0f
Vignesh061 <vigneshvicky3611@gmail.com>
2026-02-01 22:22:42 +0530

    Implemented Problem in List

---
+++ 07.Collections/01.List/Problems/08.Problem.py
@@ -0,0 +1,11 @@
+# Remove the element in the list
+
+num1=[1,2,3,4,5,6]
+
+num1.remove(2) # Remove the element by using the value
+
+num2=[0,9,8,7,6]
+num2.pop(2) # Remove the element by using the index
+
+print(num1)
+print(num2) 
