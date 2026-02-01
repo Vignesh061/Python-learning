de65d109615f250d9dadec8282edb650293523c4
Vignesh061 <vigneshvicky3611@gmail.com>
2026-02-01 22:23:28 +0530

    Implemented Problem in Nested for loop

---
+++ 06.Looping_Statements/03.Nested_for_loop/Example_Problems/05.Problem.py
@@ -0,0 +1,10 @@
+# for i in range(1,6):
+#     for j in range(i):
+#         print("*",end=" ")
+#     print()
+    
+n=6
+for k in range(1,n):
+    for l in range(n-k):
+        print(l ,end=" ")
+    print()
